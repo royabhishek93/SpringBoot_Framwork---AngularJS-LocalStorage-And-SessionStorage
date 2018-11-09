c9e871160dc8066b1eb8ab2886955976056cf75c
Abhishek Roy <abhishek.remu@gmail.com>
2018-11-09 21:40:45 +0530

    SpringBoot_Framwork---AngularJS-LocalStorage-And-SessionStorage

---
+++ AngularJSLocalStorageAndSessionStorage/src/main/java/com/abhishek/app/AngularJsLocalStorageAndSessionStorageApplication.java
@@ -0,0 +1,12 @@
+package com.abhishek.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJsLocalStorageAndSessionStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJsLocalStorageAndSessionStorageApplication.class, args);
+	}
+}
---
+++ AngularJSLocalStorageAndSessionStorage/src/main/java/com/abhishek/app/Webcontroller.java
@@ -0,0 +1,8 @@
+package com.abhishek.app;
+
+public class Webcontroller {
+	public String home() {
+		return "Index";
+	}
+
+}
